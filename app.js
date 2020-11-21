76d8a4736a570261be8fbd5c6e2cfd26e59c98de
April Long <april11dance@gmail.com>
2020-11-20 22:48:42 -0700

    fine tuning

---
+++ app.js
@@ -0,0 +1,6 @@
+
+console.log("Hello World");
+var time= new Date();
+var date= document.getElementById("dateNow"); 
+date.innerHTML= time.toLocaleTimeString();
+// date.innerHTML= time.toLocaleDateString();
